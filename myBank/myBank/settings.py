5670b8aaf6f1b27fc4676f1ab7d8d196d3a6efc2
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-06-08 23:35:00 +0100

    settings

--- myBank/myBank/settings.py
+++ myBank/myBank/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'accounts',
+    'USSD',
+    'mobileApp',
+    'transactions',
+    'staff',
 ]
 
 MIDDLEWARE = [
