325a8f44520c6b4c9218640c618cec7a5bf4c0af
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-06-08 23:34:47 +0100

    del

--- myBank/accounts/models.py
+++ myBank/accounts/models.py
@@ -1,20 +1,22 @@
 from django.db import models
-from myBank.users.models import User
+from users.models import User
+from staff.models import AccountOfficer
+from .helper_functions import calculate_account_balance
 
 # Create your models here.
 
-
-# Individual Account Types
-class SavingsAccount(models.Model):
+class Account(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='account')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='accounts')
+    account_officer = models.ForeignKey(AccountOfficer, on_delete=models.SET_NULL,
+                                        null=True, blank=True, related_name='accounts')
     account_number = models.CharField(max_length=10, unique=True)
-    account_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     account_level = models.CharField(default='1', choices=[
         ('1', 'Level 1'),
         ('2', 'Level 2'),
         ('3', 'Level 3'),
     ])
+    CLASS = models.JSONField(blank=False)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
@@ -22,21 +24,13 @@ class SavingsAccount(models.Model):
     is_PND_active = models.BooleanField(default=False)
     is_dormant = models.BooleanField(default=False)
 
-    def __str__(self):
-        return f"{self.user.first_name} {self.user.last_name} - {self.account_number} ({self.account_balance})"
 
-
-class CurrentAccount(models.Model):
-    id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='current_account')
-    account_number = models.CharField(max_length=10, unique=True)
-    account_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now=True)
-
-    # Boolean fields
-    is_PND_active = models.BooleanField(default=False)
-    is_dormant = models.BooleanField(default=False)
+    @property
+    def balance(self):
+        """
+        Calculate the account balance using the helper function.
+        """
+        return calculate_account_balance(self)
 
     def __str__(self):
-        return f"{self.user.first_name} {self.user.last_name} - {self.account_number} ({self.account_balance})"
+        return f"{self.user.first_name} {self.user.last_name} - {self.account_number}"
