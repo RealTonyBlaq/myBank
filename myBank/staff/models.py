59e18d5521a9659b6083287d5a42d3914ec01b25
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-06-09 20:43:11 +0100

    update

--- myBank/staff/models.py
+++ myBank/staff/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -6,7 +7,10 @@ class AccountOfficer(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     middle_name = models.CharField(max_length=40, default='', blank=True)
-    phone_number = models.CharField(max_length=14)
-    official_email = models.EmailField(max_length=254)
+    phone_number = models.CharField(max_length=14, unique=True)
+    official_email = models.EmailField(max_length=254, unique=True)
     employee_id = models.AutoField(primary_key=True)
     department = models.CharField(max_length=50, null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    updated_at = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
