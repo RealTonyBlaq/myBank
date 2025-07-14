e65664561d1c6145f7bc3bd2034d2530ae52dde2
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-07-14 22:48:49 +0100

    added config support from .env

--- myBank/utilities/db/redis.py
+++ myBank/utilities/db/redis.py
@@ -1,5 +1,13 @@
+from dotenv import load_dotenv, find_dotenv
+from os import getenv
 import redis
 from typing import Optional
+
+
+# Load environment variables from .env file
+load_dotenv(find_dotenv())
+
+expiry_time = int(getenv("REDIS_EXPIRY_TIME", 4))  # in minutes
 
 
 class Cache:
@@ -13,7 +21,7 @@ class Cache:
         """ Retrieves value using a key """
         value = self.client.get(key)
 
-        return value.decode('utf-8') if value else None
+        return value.decode('utf-8') if value else None # type: ignore
 
     def set(self, key: str, value: str, expires_after: int) -> None:
         """ Set/store a value using a key and an expiry time coverted to seconds """
