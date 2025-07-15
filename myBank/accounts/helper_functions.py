862beda50e1188e9d7d475c22274873f2b2bcd54
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-07-15 18:21:12 +0100

    update

--- myBank/accounts/helper_functions.py
+++ myBank/accounts/helper_functions.py
@@ -1,26 +1,5 @@
 import random
-
-
-ACCOUNT_DEFAULTS = {
-    'SAVINGS': {
-        'PREFIX': '400',
-        'ACC_CODE': 'CUSACC',
-        'ACC_TYPE': 'SAVINGS',
-        'ACC_DETAIL': 'INDIVIDUAL SAVINGS ACCOUNT'
-    },
-    'CURRENT': {
-        'PREFIX': '410',
-        'ACC_CODE': 'CUCACC',
-        'ACC_TYPE': 'CURRENT',
-        'ACC_DETAIL': 'INDIVIDUAL CURRENT ACCOUNT'
-    },
-    'FIXED': {
-        'PREFIX': '411',
-        'ACC_CODE': 'CUFACC',
-        'ACC_TYPE': 'FIXED',
-        'ACC_DETAIL': 'INDIVIDUAL FIXED DEPOSIT ACCOUNT'
-    }
-}
+from utilities.defaults import get_account_defaults, set_account_defaults
 
 
 def calculate_account_balance(account) -> float:
@@ -43,6 +22,7 @@ def calculate_account_balance(account) -> float:
     return balance
     # return 0.0
 
+
 def generate_account_number(account_type: str) -> tuple[str, dict]:
     """
     Generate a unique account number.
@@ -51,9 +31,19 @@ def generate_account_number(account_type: str) -> tuple[str, dict]:
         str: A unique account number.
     """
     from .models import Account
-    prefix = ACCOUNT_DEFAULTS.get(account_type.upper(), {}).get('PREFIX', '400') # default to SAVINGS prefix if not found
+
+    set_account_defaults() # Ensure defaults are set before generating account number
+    if not account_type:
+        raise ValueError("Account type must be specified.")
+
+    try:
+        ACCOUNT_DEFAULTS = get_account_defaults(account_type)
+    except ValueError:
+        raise ValueError(f"No defaults found for account type: {account_type}")
+
+    prefix = ACCOUNT_DEFAULTS.get('PREFIX')
     while True:
         suffix = ''.join(random.choices('0123456789', k=7))
         account_number = f"{prefix}{suffix}"
         if not Account.objects.filter(account_number=account_number).exists():
-            return account_number, ACCOUNT_DEFAULTS.get(account_type.upper(), ACCOUNT_DEFAULTS['SAVINGS'])
+            return account_number, ACCOUNT_DEFAULTS
