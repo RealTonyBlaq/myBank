e00698591c9ce02ad5cab97f5a6eb21cf8c3bf2a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-07-15 18:22:15 +0100

    added fields: grade, role. and to_dict()

--- myBank/staff/models.py
+++ myBank/staff/models.py
@@ -7,7 +7,8 @@ class AccountOfficer(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     middle_name = models.CharField(max_length=40, default='', blank=True)
-    grade = models.CharField(max_length=20)
+    grade = models.CharField(max_length=20, null=True)
+    role = models.CharField(max_length=30, null=True)
     phone_number = models.CharField(max_length=14, unique=True)
     official_email = models.EmailField(max_length=254, unique=True)
     employee_id = models.AutoField(primary_key=True)
@@ -15,3 +16,22 @@ class AccountOfficer(models.Model):
     created_at = models.DateTimeField(default=timezone.now, editable=False)
     updated_at = models.DateTimeField(default=timezone.now)
     is_active = models.BooleanField(default=True)
+
+    def to_dict(self):
+        """
+        Convert the AccountOfficer instance to a dictionary.
+        """
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'middle_name': self.middle_name,
+            'grade': self.grade,
+            'role': self.role,
+            'phone_number': self.phone_number,
+            'official_email': self.official_email,
+            'employee_id': self.employee_id,
+            'department': self.department,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat(),
+            'is_active': self.is_active
+        }
