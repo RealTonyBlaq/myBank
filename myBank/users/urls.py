86892618253e1dd3f69554d94b8eaa017bdffd70
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-07-20 21:07:01 +0100

    added: 2 new paths

--- myBank/users/urls.py
+++ myBank/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import create_user, home
 
 urlpatterns = [
-    path('', create_user, name='create_user'),
-    path('home/', home, name='home')
+    path('create', create_user, name='create_user'),
+    path('home', home, name='home')
 ]
