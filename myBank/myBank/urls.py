2424bdf78628eb3c8d52715bd1e2bc337b34a35d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-07-20 21:02:06 +0100

    update: 3 new paths

--- myBank/myBank/urls.py
+++ myBank/myBank/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('staff/', include('staff.urls'))
+    path('staff/', include('staff.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('tx/', include('transactions.urls')),
 ]
