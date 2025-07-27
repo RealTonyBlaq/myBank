bef2124af24e8f6f4c5e1411f57353c4249f1568
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-07-27 13:03:05 +0100

    added new paths

--- myBank/accounts/urls.py
+++ myBank/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import info, create_account
+from .views import info, create_account, change_dormant_status, change_PND_status, upgrade_account
 
 urlpatterns = [
     path('query/<str:account_number>', info, name='account_info'),
     path('create/individual', create_account, name='create_account'),
+    path('upgrade/<str:account_number>', upgrade_account, name='upgrade_account'),
+    path('change_pnd/<str:account_number>', change_PND_status, name='change_pnd_status'),
+    path('change_dormant/<str:account_number>', change_dormant_status, name='change_dormant_status'),
 ]
