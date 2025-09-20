a95a32105a99516b6fd1315b0ef043a2ba22fad2
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2025-09-20 20:21:48 +0100

    fix

--- myBank/accounts/views.py
+++ myBank/accounts/views.py
@@ -1,9 +1,10 @@
+from datetime import datetime
 from users.models import User
 from staff.models import AccountOfficer
 from .models import Account
 from .helper_functions import generate_account_number
 from django.http import JsonResponse
-from django.views.decorators.http import require_GET, require_POST
+from django.views.decorators.http import require_GET, require_POST, require_http_methods
 import json
 
 
@@ -61,3 +62,62 @@ def create_account(request):
                              'data': account.to_dict()}, status=201)
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=400)
+
+
+@require_http_methods(["PUT"])
+def upgrade_account(request, account_number):
+    """ Upgrade an existing account """
+    try:
+        account = Account.objects.get(account_number=account_number)
+    except Account.DoesNotExist:
+        return JsonResponse({'error': 'Account not found'}, status=404)
+
+    data = json.loads(request.body.decode('utf-8'))
+    if 'account_level' not in data:
+        return JsonResponse({'error': 'account_level is required.'}, status=400)
+    # check if the new account level is valid
+    new_account_level = data.get('account_level')
+    if new_account_level not in ['1', '2', '3']:
+        return JsonResponse({'error': 'Invalid account level.'}, status=400)
+
+    old_account_level = account.account_level
+    if old_account_level == new_account_level:
+        return JsonResponse({'error': 'Account is already at this level.'}, status=400)
+    elif int(old_account_level) > int(new_account_level):
+        return JsonResponse({'error': 'Cannot downgrade account level.'}, status=400)
+
+    account.account_level = new_account_level
+    account.date_updated = datetime.now()
+    account.save()
+    return JsonResponse({'message': 'Account upgraded successfully', 'data': account.to_dict()},
+                        status=200)
+
+
+@require_http_methods(["PUT"])
+def change_PND_status(request, account_number):
+    """ Change the PND status of an account """
+    try:
+        account = Account.objects.get(account_number=account_number)
+    except Account.DoesNotExist:
+        return JsonResponse({'error': 'Account not found'}, status=404)
+
+    account.is_PND_active = True if not account.is_PND_active else False
+    account.date_updated = datetime.now()
+    account.save()
+    return JsonResponse({'message': 'PND status updated', 'data': account.to_dict()},
+                        status=200)
+
+
+@require_http_methods(["PUT"])
+def change_dormant_status(request, account_number):
+    """ Change the dormant status of an account """
+    try:
+        account = Account.objects.get(account_number=account_number)
+    except Account.DoesNotExist:
+        return JsonResponse({'error': 'Account not found'}, status=404)
+
+    account.is_dormant = True if not account.is_dormant else False
+    account.date_updated = datetime.now()
+    account.save()
+    return JsonResponse({'message': 'Dormant status updated', 'data': account.to_dict()},
+                        status=200)
